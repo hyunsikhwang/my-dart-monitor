5e644bbbbdd19bbc78698e43383f5593e4ebc6c6
Hyunsik <gomting@naver.com>
2026-01-05 22:01:32 +0900

    debug telegram sendmessage

--- main.py
+++ main.py
@@ -207,6 +207,7 @@ def analyze_content(row):
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {'chat_id': TELEGRAM_CHAT_ID, 'text': message, 'parse_mode': 'Markdown'}
+    print(url, payload)
     requests.post(url, data=payload)
 
 # --- 메인 실행부 ---
